078693d8707d4462e2e8c3baeebf91f755012257
Denis Zinoviev <Examle20@yandex.ru>
2021-03-21 22:34:57 +0300

    fix: первый автотест

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import AddPlacePopup from "./AddPlacePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
 import CardDeletePopup from "./CardDeletePopup";
 import {CurrentUserContext} from "../contexts/CurrentUserContext";
-import api from "../utils/Api";
+import api from "../utils/api";
 
 function App() {
 
